d67cf29051a225a9764bf8829bd05a6330d0154f
gurvirsasan <70930457+gurvirsasan@users.noreply.github.com>
2025-03-24 19:55:02 -0400

    removed toggle theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from '../context/ThemeContext';
-import ThemeToggle from '../components/ThemeToggle/ThemeToggle';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-      <ThemeToggle />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
