5fe8eb1804f5c5a95866f0624ab55c489d69c2ab
gurvirsasan <70930457+gurvirsasan@users.noreply.github.com>
2025-03-24 22:40:34 -0400

    Added contacts

--- components/sections/Contact/ContactSection.tsx
+++ components/sections/Contact/ContactSection.tsx
@@ -1,10 +1,26 @@
+import GradientButton from '../../GradientButton/GradientButton';
 import SectionCard from '../../SectionCard/SectionCard';
 
 export default function ContactSection() {
   return (
-    <SectionCard id="contact" title="Contact Me">
+    <SectionCard id="contact" title="Contact Me" noBackground={true}>
       I'm always interested in hearing about new projects and opportunities.
-      Feel free to reach out to me through email or social media.
+      Feel free to reach out to me through email.
+      <div className="flex justify-center mt-80">
+        <GradientButton href="mailto:gurvirsasan1@gmail.com">
+          Email Me
+        </GradientButton>
+        <GradientButton
+          href="https://www.linkedin.com/in/gurvirsasan"
+        >
+          LinkedIn
+        </GradientButton>
+        <GradientButton
+          href="https://www.github.com/gurvirsasan"
+        >
+          GitHub
+        </GradientButton>
+      </div>
     </SectionCard>
   );
-} 
+}
