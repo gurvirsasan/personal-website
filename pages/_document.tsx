698c766c7f89e499c8ae09ab7e5d6cec3233d2ce
gurvirsasan <70930457+gurvirsasan@users.noreply.github.com>
2025-03-25 15:04:36 -0400

    add meta info

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,29 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/gurvirlogo.png" />
         <link rel="apple-touch-icon" href="/gurvirlogo.png" />
-        <meta name="description" content="Personal website of Gurvir Sasan - Software Engineer" />
+        <meta
+          name="description"
+          content="Personal website of Gurvir Sasan - Software Engineer"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
+
+        {/* Open Graph meta tags */}
+        <meta property="og:title" content="Gurvir Sasan - Software Engineer" />
+        <meta
+          property="og:description"
+          content="Check out my portfolio and explore my projects and experiences!"
+        />
+        <meta
+          property="og:image"
+          content="/thumbnail.png"
+        />
+        <meta property="og:url" content="https://gurvirsasan.vercel.app" />
+        <meta property="og:type" content="website" />
+
+        
       </Head>
+
       <body>
         <Main />
         <NextScript />
